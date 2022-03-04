f01d4b8d9424be7282743af91fb993c5e4a560f2
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-03-05 01:12:52 +0545

    Add dynamic socket url

--- src/app/utils/Socket.ts
+++ src/app/utils/Socket.ts
@@ -1,7 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
-  autoConnect: false,
-});
+const isProduction = process.env.REACT_APP_NODE_ENV === "production";
+const socket = io(
+  isProduction ? "https://writepad-api.herokuapp.com" : "http://localhost:4000",
+  {
+    autoConnect: false,
+  }
+);
 
 export default socket;
