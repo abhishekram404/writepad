ddd7faa0e75f3b7d4ef1931cfc7ec039b8ddc956
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-03-04 13:53:29 +0545

    Create Writepad component

---
+++ src/app/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Writepad from "./Writepad";
+
+function App() {
+  return (
+    <div className="App min-h-screen">
+      <Writepad />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/components/Writepad.tsx
@@ -0,0 +1,36 @@
+import React, { useRef, useState } from "react";
+type Props = {};
+
+export default function Writepad({}: Props) {
+  const [text, setText] = useState("");
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+    autoScroll(e);
+  };
+
+  const textarea = useRef<any>();
+
+  function autoScroll(e: any) {
+    e.target.style.height = "inherit";
+    e.target.style.height = `${e.target.scrollHeight}px`;
+    textarea.current.scrollTop = textarea.current.scrollHeight;
+  }
+  return (
+    <div className="flex justify-center flex-col w-[80%] mx-auto py-10">
+      <h5 className="text-3xl text-slate-800 font-mono font-bold mb-1 ">
+        Writepad
+      </h5>
+      <p className="text-xl text-slate-700 mb-4 font-mono">
+        Realtime text based collaboration environment
+      </p>
+      <textarea
+        name="writepad"
+        id="writepad"
+        className="border border-slate-400 outline-none transition-all leading-7 border-1 w-full p-4"
+        onChange={handleTextChange}
+        value={text}
+        ref={textarea}
+      ></textarea>
+    </div>
+  );
+}
