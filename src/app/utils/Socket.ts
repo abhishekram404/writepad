2b5d9521716194bc9ecc640464b4f41853ce2365
Keshav Kishor Ram <55845306+abhishekram404@users.noreply.github.com>
2022-11-12 16:14:34 +0545

    Update Socket.ts

--- src/app/utils/Socket.ts
+++ src/app/utils/Socket.ts
@@ -1,15 +1,10 @@
 import { io } from "socket.io-client";
 
-const isProduction = process.env.REACT_APP_NODE_ENV === "production";
+
 const socket = io(
-  isProduction ? "https://writepad-api.herokuapp.com" : "http://localhost:4000",
+  process.env.SOCKET_URI,
   {
     autoConnect: false,
-    extraHeaders: {
-      "Access-Control-Allow-Origin": isProduction
-        ? "https://abhishekram-404-writepad.netlify.app"
-        : "http://localhost:3000",
-    },
   }
 );
 
